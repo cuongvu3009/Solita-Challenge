d163118be5f7053b8fdabb221de988cd857a599d
cuongvu3009 <cuongvu3009@gmail.com>
2022-07-31 23:36:36 +0300

    create station

--- controllers/stationsController.js
+++ controllers/stationsController.js
@@ -1,9 +1,19 @@
+const Station = require('../models/Station');
+
+const createStation = async (req, res) => {
+  const station = await Station.create(req.body);
+  res.status(200).json(station);
+};
+
 const getAllStations = async (req, res) => {
-  res.send('All stations');
+  const stations = await Station.find({});
+  res.status(200).json(stations);
 };
 
 const getSingleStation = async (req, res) => {
-  res.send('Get single station');
+  const { id } = req.params;
+  const station = await Station.findOne({ _id: id });
+  res.status(200).json(station);
 };
 
-module.exports = { getAllStations, getSingleStation };
+module.exports = { getAllStations, getSingleStation, createStation };
--- routes/stationsRoutes.js
+++ routes/stationsRoutes.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const {
   getAllStations,
   getSingleStation,
+  createStation,
 } = require('../controllers/stationsController');
 
-router.route('/').get(getAllStations);
+router.route('/').get(getAllStations).post(createStation);
 router.route('/:id').get(getSingleStation);
 
 module.exports = router;
